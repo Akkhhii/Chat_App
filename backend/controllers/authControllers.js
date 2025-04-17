ff9c6f97a7a154875dbe6ad7f09c4e746ac8c2bd
Akkhhii <61088948+Akkhhii@users.noreply.github.com>
2025-04-17 14:12:13 +0530

    save returned token

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -65,7 +65,7 @@ const login = async (req, res)=>{
             return res.status(400).json({error : 'Invalid username or password'});
         }
 
-        generateTokenAndSetCookie(user._id, res);
+        const token = generateTokenAndSetCookie(user._id, res);
 
         console.log("Generated Token:", token);
 
