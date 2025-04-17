ce45b1f11767975891f888d7e3f4e6e633292497
Akkhhii <61088948+Akkhhii@users.noreply.github.com>
2025-04-17 13:03:38 +0530

    adding headers in /api/users route frontend side

--- frontend/chatapp/src/hooks/useGetConversation.js
+++ frontend/chatapp/src/hooks/useGetConversation.js
@@ -9,7 +9,12 @@ const useGetConversation = () => {
     const getConversations = async ()=>{
         setLoading(true)
         try {
-            const res = await fetch('https://chatbuddy-zf63.onrender.com/api/users');
+            const res = await fetch('https://chatbuddy-zf63.onrender.com/api/users',{
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}` // or sessionStorage if that's where you're storing it
+                  }
+            });
             const data = await res.json()
             if(data.error){
                 throw new Error(data.error)
