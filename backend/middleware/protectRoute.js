6caf269728e19630da8273b6d1ba458b3873691b
Akkhhii <61088948+Akkhhii@users.noreply.github.com>
2025-04-17 13:16:39 +0530

    Fix getting token from headers

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -4,7 +4,7 @@ const User = require('../model/user')
 
 const protectRoute = async (req, res, next)=>{
     try {
-        const token = req.cookies.jwt;
+        const token = req.cookies.jwt || req.headers['authorization']?.split(' ')[1];;
   
         if(!token){
             return res.status(401).json({error : 'Unauthorized - No Token provided'});
